2eed0209e5aad86e54258898d63bfb80e0478a2d
juandevsigner <juanda.core07@gmail.com>
2023-01-06 11:32:47 -0500

    feat: mongoose + product models

---
+++ models/Product.ts
@@ -0,0 +1,63 @@
+import mongoose, { Schema, model, Model } from "mongoose";
+import { IProduct } from "../interfaces";
+
+const ProductSchema = new Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    images: [{ type: String }],
+    inStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    sizes: [
+      {
+        type: String,
+        enum: {
+          values: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"],
+          message: "{VALUE} is not valid size",
+        },
+      },
+    ],
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    tags: [{ type: String }],
+    title: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: {
+        values: ["shirts", "pants", "hoodies", "hats"],
+        message: "{VALUE} is not valid type",
+      },
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ["men", "women", "kid", "unisex"],
+        message: "{VALUE} is not valid gender",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//TODO: crear indice de
+const Product: Model<IProduct> =
+  mongoose.models.Product || model("Product", ProductSchema);
+export default Product;
