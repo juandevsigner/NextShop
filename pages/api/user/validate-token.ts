e84ddcb4009e249b031f9dc5d4f8bf537595f9e2
juandevsigner <juanda.core07@gmail.com>
2023-01-11 07:49:01 -0500

    feat: revalidate token

---
+++ pages/api/user/validate-token.ts
@@ -0,0 +1,65 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcryptjs";
+
+import { db } from "../../../database";
+import { User } from "../../../models";
+import { jwt } from "../../../utils";
+
+type Data =
+  | { message: string }
+  | {
+      token: string;
+      user: {
+        email: string;
+        name: string;
+        role: string;
+      };
+    };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case "GET":
+      return checkJWT(req, res);
+
+    default:
+      res.status(400).json({
+        message: "Bad request",
+      });
+  }
+}
+
+const checkJWT = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { token = "" } = req.headers;
+
+  let userId = "";
+
+  try {
+    userId = await jwt.isValidToken(token.toString());
+  } catch (error) {
+    return res.status(401).json({
+      message: "Token not valid",
+    });
+  }
+
+  await db.connect();
+  const user = await User.findById(userId).lean();
+  await db.disconnect();
+
+  if (!user) {
+    return res.status(400).json({ message: "USer doest no exist" });
+  }
+
+  const { _id, email, role, name } = user;
+
+  return res.status(200).json({
+    token: jwt.signToken(_id, email),
+    user: {
+      email,
+      role,
+      name,
+    },
+  });
+};
--- utils/jwt.ts
+++ utils/jwt.ts
@@ -17,3 +17,23 @@ export const signToken = (_id: string, email: string) => {
     }
   );
 };
+
+export const isValidToken = (token: string): Promise<string> => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error(
+      "Dont have a secret seed JWT, please check your environment"
+    );
+  }
+  return new Promise((resolve, reject) => {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET_SEED || "", (err, payload) => {
+        if (err) return reject("Token no valid");
+
+        const { _id } = payload as { _id: string };
+        resolve(_id);
+      });
+    } catch (error) {
+      reject("Token no valid");
+    }
+  });
+};
