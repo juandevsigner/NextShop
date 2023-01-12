d169f32c97f5034458e96751694186ffe4fdcfd3
juandevsigner <juanda.core07@gmail.com>
2023-01-12 12:43:55 -0500

    feat: middleware integration

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!session) {
+    const requestedPage = req.nextUrl.pathname;
+    const url = req.nextUrl.clone();
+    url.pathname = `/auth/login`;
+    url.search = `p=${requestedPage}`;
+
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+// Supports both a single string value or an array of matchers
+export const config = {
+  matcher: ["/checkout/address", "/checkout/summary"],
+};
